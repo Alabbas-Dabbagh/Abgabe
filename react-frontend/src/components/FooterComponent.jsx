cadae7d0df0fc8742463542071e927d1d7728d19
Alabbas-Dabbagh <dabbagh.alabbas@hotmail.com>
2023-01-13 11:29:21 +0100

    Referat

---
+++ react-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class FooterComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <footer class = "footer">
+                    <span className="text-muted">Alabbas Dabbagh</span>
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default FooterComponent
